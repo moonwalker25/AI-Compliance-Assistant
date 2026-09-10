8d850dd566360b97b538c97b53f69d8ce6e62f90
HARSHITA TALWAR <paritoshtalwar@Harshitas-MacBook-Air.local>
2026-09-10 17:28:27 +0530

    Set chatbot as default route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 
-import Home from "./pages/Home";
 import Chat from "./pages/Chat";
 import Admin from "./pages/Admin";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/chat" element={<Chat />} />
+      <Route path="/" element={<Chat />} />
       <Route path="/admin" element={<Admin />} />
     </Routes>
   );
